d8a34e8e04c9f37ae18995674840e798c3ba8ec5
thiagoaugust <thiago.programador.net@gmail.com>
2019-02-13 15:51:54 -0200

    Adcionando novas funcionalidades

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
 var tempoInicial = $("#contador-digitacao").text();
-var campo = $("#campo-digitacao");
+var campo = $(".campo-digitacao");
 
 $(document).ready(function(){
 	atualizarTamanhoFrase();
 	inicializaContadores();
 	inicializaCronometro();
+	inicializaTesteCampo();
 	$("#botao-reiniciar").click(reiniciaJogo);
 });	
 
@@ -27,6 +28,21 @@ function inicializaContadores(){
 	});
 }
 
+function inicializaTesteCampo(){
+	var frase = $(".frase").text();
+	campo.on("input", function(){
+		var digitado = campo.val();
+		var comparavel = frase.substr(0,digitado.length);
+		if(digitado==comparavel){
+			campo.addClass("borda-verde");
+			campo.removeClass("borda-vermelha");
+		}else{
+			campo.addClass("borda-vermelha");
+			campo.removeClass("borda-verde");
+		}
+	});
+}
+
 
 function inicializaCronometro(){
 	var tempoRestante = $("#contador-digitacao").text();
@@ -35,17 +51,52 @@ function inicializaCronometro(){
 			tempoRestante--;
 			$("#contador-digitacao").text(tempoRestante);
 			if(tempoRestante<1){
+				clearInterval(idContador);
 				campo.attr("disabled",true);
 				campo.addClass("campo-desativado");
-				clearInterval(idContador);
-				console.log(tempoRestante);
+				inserePlacar();
 			}
 		},1000)
 	});
 }
 
+function inserePlacar(){
+	var corpoTabela = $(".placar").find("tbody");
+	var usuario = "Thiagao";
+	var numPalavras = $("#contador-palavras").text();
+	
+	var linha = novaLinha(usuario,numPalavras);
+	linha.find(".botao-remover").click(removeLinha);
+	corpoTabela.prepend(linha);
+}
+
+function novaLinha(usuario,numPalavras){
+	var linha = $("<tr>");
+	var colunaUsuario = $("<td>").text(usuario);
+	var colunaPalavras = $("<td>").text(numPalavras);
+	var colunaRemover = $("<td>");
+	var tagA = $("<a>").addClass("botao-remover").attr("href","#");
+	var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+	tagA.append(icone);
+	colunaRemover.append(tagA);
+	linha.append(colunaUsuario);
+	linha.append(colunaPalavras);
+	linha.append(colunaRemover);
+
+	return linha
+}
+
+function removeLinha(){
+	event.preventDefault();
+	$(this).parent().parent().remove();
+
+}
+
 function reiniciaJogo(){
 	campo.removeClass("campo-desativado");
+	campo.removeClass("borda-verde");
+	campo.removeClass("borda-vermelha");
 	campo.val("");
 	campo.attr("disabled",false);
 	$("#contador-palavras").text("0");
